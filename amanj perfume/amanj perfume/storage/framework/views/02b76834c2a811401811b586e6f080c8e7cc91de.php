ecd5ace9d22c026d8cdde3579facb9e7ac54ffcc
Ali <101445195+minagar1380@users.noreply.github.com>
2025-01-07 12:35:44 +0330

    Delete amanj perfume directory

--- amanj perfume/amanj perfume/storage/framework/views/02b76834c2a811401811b586e6f080c8e7cc91de.php
+++
@@ -1,9 +0,0 @@
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<link rel="stylesheet" href="<?php echo e(asset('customer-assets/css/bootstrap/bootstrap-reboot.rtl.min.css')); ?>">
-<link rel="stylesheet" href="<?php echo e(asset('customer-assets/css/bootstrap/bootstrap.rtl.min.css')); ?>">
-<link rel="stylesheet" href="<?php echo e(asset('customer-assets/fontawesome/css/all.min.css')); ?>"
-<title>فروشگاه آمازون</title>
-<?php /**PATH /Users/apple/Desktop/desk/project۰org/resources/views/emails/layouts/head-tag.blade.php ENDPATH**/ ?>
